2323ef4b0e39ee4814bd3406155c4e7a74ece67e
Chloe Shetreet <CourteousCoder@gmail.com>
2024-08-07 00:47:33 -0700

    2024-08-07 00:47 : id 9 -> /nix/store/qghgyaydj4cqdzkb3691n6wfyfl3zipk-home-manager-generation

--- home.nix
+++ home.nix
@@ -59,7 +59,7 @@
     taskwarrior
     tmsu
     thefuck
-
+    nautilus
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
