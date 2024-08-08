1e8f7f084c8544686aa642d0ee48b0afb63e3270
Chloe Shetreet <CourteousCoder@gmail.com>
2024-08-07 19:34:46 -0700

    2024-08-07 19:34 : id 11 -> /nix/store/1a3skv23znrbfv8gxxcj8hwvfkdn7bya-home-manager-generation

--- home.nix
+++ home.nix
@@ -82,7 +82,7 @@
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+#     ".config/fish".source = dotfiles/.config/fish/config.fish
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
@@ -109,6 +109,7 @@
   #
   home.sessionVariables = {
     EDITOR = "lvim";
+    VISUAL = "codium";
     PAGER = "bat";
   };
 
