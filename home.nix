64e15145e35cbd0aa42fb0b34826f48777d09a99
Chloe Shetreet <CourteousCoder@gmail.com>
2024-08-09 00:01:04 -0700

    2024-08-09 00:01 : id 18 -> /nix/store/c02lg6gpqndx7kip2qvzk7z2z2a4f0qb-home-manager-generation

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    librewolf
     neofetch
     discord
     bitwarden
@@ -61,7 +62,7 @@
     thefuck
     nautilus
     pcmanfm
-    oksh
+    syncthing
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
