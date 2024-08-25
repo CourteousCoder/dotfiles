f963bfcf182fcc87788039ff6f96a8085042dac3
Chloe Shetreet <CourteousCoder@gmail.com>
2024-08-24 22:32:01 -0700

    2024-08-24 22:32 : id 31 -> /nix/store/npwrgq6jp52dw27js5kxf5jp3x4ssfkj-home-manager-generation

--- home.nix
+++ home.nix
@@ -65,7 +65,6 @@
     nautilus
     pcmanfm
     kitty
-    alacritty
     syncthing
     asciinema
     #nixgl
@@ -79,9 +78,32 @@
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
-    #(pkgs.writeShellScriptBin "nh-add" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    (pkgs.writeShellScriptBin "alacritty" ''
+     nix run github:nix-community/nixGL#nixGLIntel -- nix run nixpkgs#alacritty & 
+    '')
+      
+    (pkgs.writeShellScriptBin "home-rebuild.sh" ''
+    #!/usr/bin/env bash
+
+    set -e
+
+    export EDITOR=${EDITOR:-lvim}
+    dotfiles=~/dotfiles
+
+    pushd "$dotfiles"
+
+    home-manager edit
+    git diff -U0 *.nix
+
+    echo "Rebuilding Nix home manager..."
+    nh home switch "$dotfiles" &>nh-home-switch.log || (
+      cat nh-home-switch.log | grep --color error && false)
+
+    gen="$(home-manager generations | head -n 1)"
+    git commit -am "$gen"
+
+    popd
+    '')
   ];
   # ++ import ./packages.nix { inherit pkgs; };
 
