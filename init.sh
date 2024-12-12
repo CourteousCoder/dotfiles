507b57ad10ac1bc2ee70a540c28af404c429d531
Chloe Shetreet <CourteousCoder@gmail.com>
2024-12-12 04:47:21 -0800

    Update init script to use lix for nix distro, and avoids piping to sh.

--- init.sh
+++ init.sh
@@ -8,11 +8,11 @@ LOCAL_FLAKE=~/.dotfiles
 
 if ! command -v nix; then
   echo installing nix
-  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+  wget https://install.lix.systems/lix &&  sh ./lix -- install && rm ./lix
+  #curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 fi
 
 test -d "$LOCAL_FLAKE" || nix flake clone "$REMOTE_REPO" --dest "$LOCAL_FLAKE"
-pushd "$LOCAL_FLAKE"
+cd "$LOCAL_FLAKE"
 nix run nixpkgs#git -- pull 
 nix run home-manager/master -- switch --flake .
-popd
