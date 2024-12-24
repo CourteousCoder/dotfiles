ddbe4f1d422878dcc1a127f72e603dd7426ee383
Chloe Shetreet <CourteousCoder@gmail.com>
2024-12-23 18:49:42 -0800

    2024-12-18 03:48 : id 7 -> /nix/store/c61vnba6bjb620ys9v47mmkhx0jj8xg3-home-manager-generation

--- dotfiles/_.config/fish/config.fish
+++ dotfiles/_.config/fish/config.fish
@@ -1,13 +1,14 @@
+set -g SHELL (which fish)
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	    # Commands to run in interactive sessions can go here
+	command -v thefuck > /dev/null
+	and thefuck --alias | source
+
+	command -v uv > /dev/null
+	and uv generate-shell-completion fish | source
+	command -v uvx > /dev/null
+	and uvx --generate-shell-completion fish | source
+
 end
-
-command -v thefuck > /dev/null
-and thefuck --alias | source
-
-command -v uv > /dev/null
-and uv generate-shell-completion fish | source
-command -v uvx > /dev/null
-and uvx --generate-shell-completion fish | source
 
 fish_add_path -g "$HOME/.local/share/JetBrains/Toolbox/bin"
