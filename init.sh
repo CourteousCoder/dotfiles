e2570b6353315a5284bfaea7edb73910f3d59fc4
Chloe Shetreet <CourteousCoder@gmail.com>
2025-01-15 16:46:30 -0800

    2025-01-14 17:51 : id 14 -> /nix/store/ndv49bgfr694j3v5gg62803h6zpw4rk5-home-manager-generation

--- init.sh
+++ init.sh
@@ -1,15 +1,37 @@
 #!/usr/bin/env sh
-set -e
-set -v
 
-if ! command -v nix; then
-	echo installing nix
-	export NIX_INSTALLER_ENABLE_FLAKES=true
-	wget https://install.lix.systems/lix &&  sh ./lix -- install && rm ./lix
-	#curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
-	. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-fi
+main() {
+	if ! check_cmd nix; then
+		say Installing nix
 
-git pull  --all
-nix run home-manager/master -- switch --flake .
+		# export NIX_INSTALLER_NO_CONFIRM=true
+		export NIX_INSTALLER_ENABLE_FLAKES=true
+		export NIX_INSTALLER_EXTRA_CONF='extra-trusted-users = "@wheel"'
 
+		install_nix_from 'https://install.lix.systems/lix' || install_nix_from 'https://install.determinate.systems/nix' || err "Coult not install nix"
+
+		. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+	fi
+
+	need_cmd nix
+	
+	run_or_nix_run git pull  --all
+	run_or_nix_run home-manager switch --flake .
+}
+
+install_nix_from() {
+	local _retval=true
+	local _installer=$( mktemp tmp_nix-installer_XXXX.sh )
+	ensure downloader --check
+
+	say "\t trying installer from $1" 
+	downloader "$1" "$_installer" || _retval=false
+	chmod +x "$_installer"
+	"$_installer" install || _retval=false
+	rm "$_installer"
+	return "$_retval"
+}
+
+
+
+. ./dotfiles/_.local/lib/script-utils.sh
