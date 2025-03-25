faacbb158381735b59387056ed0846f3b30eac8b
Chloe Shetreet <CourteousCoder@gmail.com>
2025-03-24 22:41:49 -0700

    2025-03-24 22:41 : id 3 -> /nix/store/jbqyn26pdvw6nv304cxhhqzs3lm9qnds-home-manager-generation

--- home.nix
+++ home.nix
@@ -34,7 +34,7 @@ in {
       # # Building this configuration will create a copy of 'dotfiles/screenrc' in
       # # the Nix store. Activating the configuration will then make '~/.screenrc' a
       # # symlink to the Nix store copy.
-      ".config/home-manager".source = config.lib.file.mkOutOfStoreSymlink myHomeManagerFlake;
+      #      ".config/home-manager".source = config.lib.file.mkOutOfStoreSymlink myHomeManagerFlake;
       # # You can also set the file content immediately.
       # ".gradle/gradle.properties".text = ''
       #   org.gradle.console=verbose
