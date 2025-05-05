346fa61c6fa94654102a3346d7071330680f68d6
Chloe Shetreet <CourteousCoder@gmail.com>
2025-05-04 18:34:50 -0700

    2025-05-04 16:53 : id 11 -> /nix/store/ywk9cx4ifnxffvrbqpn3snvmmkl31b9k-home-manager-generation

--- stow/fish/dot-config/fish/functions/fish_command_not_found.fish
+++ stow/fish/dot-config/fish/functions/fish_command_not_found.fish
@@ -1,20 +1,27 @@
 #!/usr/bin/fish
 function fish_command_not_found --wraps=__fish_default_command_not_found_handler
     __fish_default_command_not_found_handler $argv
-
-    # and Check if we opted in to this
-    contains "$(string lower "$FISH_COMMAND_NOT_FOUND_AUTO_TRY_NIXPKGS")" true 1 yes y set on enable enabled always 
-    and command -q nix
-    and command -q nc
-    and nc -z github.com 443  #
-    and test -e "~/.cache/nix-index/files"
+    true
+        and status is-interactive
+        and command -q nix
+        and test "$FISH_COMMAND_NOT_FOUND_AUTO_TRY_NIXPKGS" = true
+        and command -q nc
+        and nc -zw 1 github.com 443  #
     or return 1
 
-    echo Attempting to download and run $argv[1] from nixpkgs... > /dev/stderr
+    echo Attempting to run "'$argv[1]'" from nixpkgs
+    if not test -e "~/.cache/nix-index/files"
+        echo "Building package index..."
+        echo '  nix run nixpkgs#nix-index'
+        nix run nixpkgs#nix-index
+        or return 1
+    end
 
     if command -q comma
+        echo "  comma -- $argv"
         comma -- $argv
     else 
+        echo "  nix --extra-experimental-features 'nix-command flakes' run 'nixpkgs#comma' -- $argv"
         nix --extra-experimental-features 'nix-command flakes' run 'nixpkgs#comma' -- $argv
     end
 end
