2680b2e13d6d86f7812498da0f455392d8bbc947
Chloe Shetreet <CourteousCoder@gmail.com>
2025-07-07 20:25:06 -0700

    moved in misplaced configs

--- stow/dot-config/fish/functions/treesync.fish
+++ stow/dot-config/fish/functions/treesync.fish
@@ -1,15 +1,20 @@
 function treesync --description 'merges the directory structure under the first argument into the directory structures under each remaining argument, and without copying files'
-    if not set -q argv[2]
-        echo "need at least 2 arguments"
+    # Normalize args into distinct realpaths
+    true
+    and set -l argv (path resolve $argv | uniq -u)
+    and set -l source_dir (realpath $argv[1])
+    and set -l dest_dirs $argv[2..] 
+    echo $argv
+    and mkdir -p $argv
+    or begin
+        printf 'ERROR: source_dir must exist and dest_dirs must either be directories or non-existent'
         return 1
     end
-    set source_dir (path resolve $argv[1])
-    set dest_dirs (path resolve $argv[2..])
 
     pushd $source_dir
-    or return= 1
-    for dest in $dest_dirs 
-        find . -mindepth 2 -type d -exec mkdir -p -- $dest/{} \;
+    or return 1
+    for dest in $dest_dirs
+        find . -mindepth 2 -type d -exec mkdir -p $dest/'{}' ';'
         or continue
         echo $source_dir to $dest
     end
---
+++ stow/dot-config/nyxt/auto-config.3.lisp
@@ -0,0 +1,62 @@
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes
+    (remove-if
+     (lambda (nyxt::m)
+       (find (symbol-name nyxt::m)
+             '("EMACS-MODE" "VI-NORMAL-MODE" "VI-INSERT-MODE") :test
+             #'string=))
+     %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/blocker:blocker-mode %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes
+    (pushnew 'nyxt/mode/reduce-tracking:reduce-tracking-mode %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes
+    (remove-if
+     (lambda (nyxt::m)
+       (find (symbol-name nyxt::m)
+             '("EMACS-MODE" "VI-NORMAL-MODE" "VI-INSERT-MODE") :test
+             #'string=))
+     %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes
+    (remove-if (lambda (nyxt::m) (string= (symbol-name nyxt::m) "DARK-MODE"))
+               %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
