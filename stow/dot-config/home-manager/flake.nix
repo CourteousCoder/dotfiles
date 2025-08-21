2663474a1bb3bf0e039063547f59cca9aa8abb96
Chloe Shetreet <CourteousCoder@gmail.com>
2025-08-20 20:48:35 -0700

    2025-08-20 16:58 : id 28 -> /nix/store/fh43zw41rajy1nx4rgalwslzxv38g6rp-home-manager-generation (current)

--- stow/dot-config/home-manager/flake.nix
+++ stow/dot-config/home-manager/flake.nix
@@ -45,7 +45,6 @@
           (final: prev: {
             flox = flox.packages.${prev.system}.default;
           })
-          snowfall-flake.overlay
           snowfall-flake.overlays."package/flake"
         ];
       };
